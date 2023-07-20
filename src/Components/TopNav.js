949f7bb6c6d332780877f9a2254b612f77db0f7c
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-07-21 00:36:47 +1000

    changes.

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
+import NavBar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 
 const TopNav = () => {
   return (
-    <Nabvar expand="md" className="topNav" id="navbar">
+    <NavBar expand="md" className="topNav" id="navbar">
       <Container>
-        <Link></Link>
+        <Link to="/">Logo</Link>
+        <NavBar.Toggle aria-controls="basic-navbar-nav" />
+        <NavbarCollapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link className="nav-link" to="/skills">
+              Skills
+            </Link>
+            <Link className="nav-link" to="/projects">
+              Projects
+            </Link>
+            <Link className="nav-link" to="/about">
+              About Me
+            </Link>
+            <Link className="nav-link" to="/contact">
+              Contact
+            </Link>
+          </Nav>
+        </NavbarCollapse>
       </Container>
-    </Nabvar>
+    </NavBar>
   );
 };
 
