5c17b73f9f9d97848d5ec442c9af6b39a4d1193b
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-07-20 22:36:26 +1000

    project initialised

---
+++ src/Components/TopNav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const TopNav = () => {
+  return (
+    <Nabvar expand="md" className="topNav" id="navbar">
+      <Container>
+        <Link></Link>
+      </Container>
+    </Nabvar>
+  );
+};
+
+export default TopNav;
