f3074b0548baa7615fabcc04c696a4a0af011749
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-07-22 19:37:18 +1000

    build project section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Skills from "./Components/Skills";
 import HeroSection from "./Components/HeroSection";
+import Project from "./Components/Project";
+import AboutMe from "./Components/AboutMe";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,13 +1,137 @@
 import React from "react";
-import { Section } from "bootstrap";
+import e_commerce_cms from "../assets/e_commerce_cms.png";
 import Layout from "./Layout";
 
 const Project = () => {
   return (
     <Layout>
-      <Section className="projects">
-        <h2 className="project-title">SOme of my Recent Projects</h2>
-      </Section>
+      <div className="projects container text-center">
+        <h2 className="project-title mb-10">Some of My Recent Projects</h2>
+        <div className="projects-container row ">
+          <div className="project-container project-card col ">
+            <img
+              src={e_commerce_cms}
+              alt="e_commerce_cms"
+              className="project-pic"
+            />
+            <h3 className="project-title">E-Commerce CMS</h3>
+            <p className="projetc-details">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              earum eos soluta dignissimos dolor accusamus reprehenderit modi
+              dicta assumenda necessitatibus, illo corrupti molestiae illum
+              ipsum quibusdam sapiente, adipisci delectus et labore odit
+              voluptatibus ducimus sunt voluptatem asperiores? Dicta dolor
+              molestias iste sit rem suscipit velit dolores, assumenda accusamus
+              consequatur illo?
+            </p>
+            <a href="#" target="_blank" className=" project-link effectForLink">
+              Check it Out!
+            </a>
+          </div>
+          <div className="project-container project-card col">
+            <img
+              src={e_commerce_cms}
+              alt="e_commerce_cms"
+              className="project-pic"
+            />
+            <h3 className="project-title">E-Commerce CMS</h3>
+            <p className="projetc-details">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              earum eos soluta dignissimos dolor accusamus reprehenderit modi
+              dicta assumenda necessitatibus, illo corrupti molestiae illum
+              ipsum quibusdam sapiente, adipisci delectus et labore odit
+              voluptatibus ducimus sunt voluptatem asperiores? Dicta dolor
+              molestias iste sit rem suscipit velit dolores, assumenda accusamus
+              consequatur illo?
+            </p>
+            <a href="#" target="_blank" className="project-link effectForLink ">
+              Check it Out!
+            </a>
+          </div>
+          <div className="project-container project-card col">
+            <img
+              src={e_commerce_cms}
+              alt="e_commerce_cms"
+              className="project-pic"
+            />
+            <h3 className="project-title">E-Commerce CMS</h3>
+            <p className="projetc-details">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              earum eos soluta dignissimos dolor accusamus reprehenderit modi
+              dicta assumenda necessitatibus, illo corrupti molestiae illum
+              ipsum quibusdam sapiente, adipisci delectus et labore odit
+              voluptatibus ducimus sunt voluptatem asperiores? Dicta dolor
+              molestias iste sit rem suscipit velit dolores, assumenda accusamus
+              consequatur illo?
+            </p>
+            <a href="#" target="_blank" className="project-link effectForLink">
+              Check it Out!
+            </a>
+          </div>
+        </div>
+        <div className="projects-container row">
+          <div className="project-container project-card col">
+            <img
+              src={e_commerce_cms}
+              alt="e_commerce_cms"
+              className="project-pic"
+            />
+            <h3 className="project-title">E-Commerce CMS</h3>
+            <p className="projetc-details">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              earum eos soluta dignissimos dolor accusamus reprehenderit modi
+              dicta assumenda necessitatibus, illo corrupti molestiae illum
+              ipsum quibusdam sapiente, adipisci delectus et labore odit
+              voluptatibus ducimus sunt voluptatem asperiores? Dicta dolor
+              molestias iste sit rem suscipit velit dolores, assumenda accusamus
+              consequatur illo?
+            </p>
+            <a href="#" target="_blank" className="project-link effectForLink">
+              Check it Out!
+            </a>
+          </div>
+          <div className="project-container project-card col">
+            <img
+              src={e_commerce_cms}
+              alt="e_commerce_cms"
+              className="project-pic"
+            />
+            <h3 className="project-title">E-Commerce CMS</h3>
+            <p className="projetc-details">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              earum eos soluta dignissimos dolor accusamus reprehenderit modi
+              dicta assumenda necessitatibus, illo corrupti molestiae illum
+              ipsum quibusdam sapiente, adipisci delectus et labore odit
+              voluptatibus ducimus sunt voluptatem asperiores? Dicta dolor
+              molestias iste sit rem suscipit velit dolores, assumenda accusamus
+              consequatur illo?
+            </p>
+            <a href="#" target="_blank" className="project-link effectForLink">
+              Check it Out!
+            </a>
+          </div>
+          <div className="project-container project-card col ">
+            <img
+              src={e_commerce_cms}
+              alt="e_commerce_cms"
+              className="project-pic"
+            />
+            <h3 className="project-title">E-Commerce CMS</h3>
+            <p className="projetc-details">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              earum eos soluta dignissimos dolor accusamus reprehenderit modi
+              dicta assumenda necessitatibus, illo corrupti molestiae illum
+              ipsum quibusdam sapiente, adipisci delectus et labore odit
+              voluptatibus ducimus sunt voluptatem asperiores? Dicta dolor
+              molestias iste sit rem suscipit velit dolores, assumenda accusamus
+              consequatur illo?
+            </p>
+            <a href="#" target="_blank" className="project-link  effectForLink">
+              Check it Out!
+            </a>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
